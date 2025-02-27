4f5f7780016666cb03692b1dc4c0d9e1385bfa58
Adrián Rosa <deftoneslovers22@gmail.com>
2025-02-27 15:58:45 -0600

    Changes
    xd

---
+++ Proyecto/Form3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_FINAL
+{
+    public partial class Form3 : Form
+    {
+        //
+        
+        private string[,] admin;
+        private string[] planes;
+        private int contadorSocios;
+        private string[,,] socios;
+        private List<string> listaSocios;
+        private string[] generos;
+        //
+
+        public Form3(string[,] admin, string[] planes, int contadorSocios, string[,,] socios, List<string> listaSocios, string[] generos)
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.admin = admin;
+            this.planes = planes;
+            this.contadorSocios = contadorSocios;
+            this.socios = socios;
+            this.listaSocios = listaSocios;
+            this.generos = generos;
+        }
+
+
+    }
+}
---
+++ Proyecto/RegistrarUsuarios.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Proyecto_FINAL
+{
+    partial class RegistrarUsuarios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label9 = new Label();
+            cmbGeneros = new ComboBox();
+            txtCvv = new TextBox();
+            label8 = new Label();
+            txtTarjetaCredito = new TextBox();
+            label2 = new Label();
+            txtAltura = new TextBox();
+            label3 = new Label();
+            cmbPlan = new ComboBox();
+            label7 = new Label();
+            label6 = new Label();
+            txtPeso = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label1 = new Label();
+            TxtNumero = new TextBox();
+            txtEdad = new TextBox();
+            txtNombre = new TextBox();
+            btn_Regristarse = new Button();
+            btnVolver = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(cmbGeneros);
+            groupBox1.Controls.Add(txtCvv);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(txtTarjetaCredito);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txtAltura);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(cmbPlan);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(txtPeso);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(TxtNumero);
+            groupBox1.Controls.Add(txtEdad);
+            groupBox1.Controls.Add(txtNombre);
+            groupBox1.Controls.Add(btn_Regristarse);
+            groupBox1.Location = new Point(170, 28);
+            groupBox1.Margin = new Padding(4, 3, 4, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.Size = new Size(560, 640);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(40, 461);
+            label9.Margin = new Padding(4, 0, 4, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(77, 25);
+            label9.TabIndex = 23;
+            label9.Text = "Generos";
+            // 
+            // cmbGeneros
+            // 
+            cmbGeneros.FormattingEnabled = true;
+            cmbGeneros.Location = new Point(40, 489);
+            cmbGeneros.Margin = new Padding(4, 3, 4, 3);
+            cmbGeneros.Name = "cmbGeneros";
+            cmbGeneros.Size = new Size(188, 33);
+            cmbGeneros.TabIndex = 22;
+            // 
+            // txtCvv
+            // 
+            txtCvv.Location = new Point(40, 412);
+            txtCvv.Margin = new Padding(4, 3, 4, 3);
+            txtCvv.Name = "txtCvv";
+            txtCvv.Size = new Size(188, 31);
+            txtCvv.TabIndex = 21;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(40, 383);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(41, 25);
+            label8.TabIndex = 20;
+            label8.Text = "Cvv";
+            // 
+            // txtTarjetaCredito
+            // 
+            txtTarjetaCredito.Location = new Point(40, 318);
+            txtTarjetaCredito.Margin = new Padding(4, 3, 4, 3);
+            txtTarjetaCredito.Name = "txtTarjetaCredito";
+            txtTarjetaCredito.Size = new Size(188, 31);
+            txtTarjetaCredito.TabIndex = 19;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 290);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(220, 25);
+            label2.TabIndex = 18;
+            label2.Text = "Tarjeta de credito o debito";
+            // 
+            // txtAltura
+            // 
+            txtAltura.Location = new Point(300, 95);
+            txtAltura.Margin = new Padding(4, 5, 4, 5);
+            txtAltura.Name = "txtAltura";
+            txtAltura.Size = new Size(170, 31);
+            txtAltura.TabIndex = 17;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(300, 65);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 16;
+            label3.Text = "Altura";
+            // 
+            // cmbPlan
+            // 
+            cmbPlan.FormattingEnabled = true;
+            cmbPlan.Location = new Point(300, 412);
+            cmbPlan.Margin = new Padding(4, 3, 4, 3);
+            cmbPlan.Name = "cmbPlan";
+            cmbPlan.Size = new Size(188, 33);
+            cmbPlan.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(303, 383);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 25);
+            label7.TabIndex = 14;
+            label7.Text = "Plan";
+            label7.Click += label7_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(300, 290);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 25);
+            label6.TabIndex = 12;
+            label6.Text = "Peso";
+            label6.Click += label6_Click;
+            // 
+            // txtPeso
+            // 
+            txtPeso.Location = new Point(300, 318);
+            txtPeso.Margin = new Padding(4, 3, 4, 3);
+            txtPeso.Name = "txtPeso";
+            txtPeso.Size = new Size(170, 31);
+            txtPeso.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(40, 178);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(161, 25);
+            label5.TabIndex = 10;
+            label5.Text = "Numero Telefonico";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(300, 178);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Edad";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 67);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 25);
+            label1.TabIndex = 6;
+            label1.Text = "Nombre Completo";
+            // 
+            // TxtNumero
+            // 
+            TxtNumero.Location = new Point(40, 207);
+            TxtNumero.Margin = new Padding(4, 3, 4, 3);
+            TxtNumero.Name = "TxtNumero";
+            TxtNumero.Size = new Size(188, 31);
+            TxtNumero.TabIndex = 3;
+            // 
+            // txtEdad
+            // 
+            txtEdad.Location = new Point(300, 207);
+            txtEdad.Margin = new Padding(4, 3, 4, 3);
+            txtEdad.Name = "txtEdad";
+            txtEdad.Size = new Size(170, 31);
+            txtEdad.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(40, 95);
+            txtNombre.Margin = new Padding(4, 3, 4, 3);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(188, 31);
+            txtNombre.TabIndex = 1;
+            // 
+            // btn_Regristarse
+            // 
+            btn_Regristarse.Location = new Point(210, 582);
+            btn_Regristarse.Margin = new Padding(4, 3, 4, 3);
+            btn_Regristarse.Name = "btn_Regristarse";
+            btn_Regristarse.Size = new Size(117, 37);
+            btn_Regristarse.TabIndex = 0;
+            btn_Regristarse.Text = "button1";
+            btn_Regristarse.UseVisualStyleBackColor = true;
+            btn_Regristarse.Click += btn_Regristarse_Click;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Location = new Point(802, 26);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(112, 34);
+            btnVolver.TabIndex = 1;
+            btnVolver.Text = "volver";
+            btnVolver.UseVisualStyleBackColor = true;
+            btnVolver.Click += btnVolver_Click;
+            // 
+            // RegistrarUsuarios
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1000, 747);
+            Controls.Add(btnVolver);
+            Controls.Add(groupBox1);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "RegistrarUsuarios";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox txt_correo;
+        private TextBox TxtNumero;
+        private TextBox txtEdad;
+        private TextBox txtNombre;
+        private Button btn_Regristarse;
+        private Label label5;
+        private Label label4;
+        private Label label1;
+        private Label label6;
+        private TextBox txtPeso;
+        private Label label7;
+        private ComboBox cmbPlan;
+        private TextBox txtAltura;
+        private Label label3;
+        private Label label8;
+        private TextBox txtTarjetaCredito;
+        private Label label2;
+        private TextBox txtCvv;
+        private Label label9;
+        private ComboBox cmbGeneros;
+        private Button btnVolver;
+    }
+}
